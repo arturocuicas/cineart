792081037b4bed79f2e3180cba107a820ecb970f
Arturo Cuicas <orutrax@gmail.com>
2020-07-04 18:25:53 -0400

    Add styles and restriction

--- cineart/movies/views.py
+++ cineart/movies/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from cineart.movies.models import Movie
 
 
+@login_required
 def list_movies(request):
-    "List of Movies view"
+    """List of Movies view"""
 
     movies = Movie.objects.all()
 
@@ -16,6 +18,7 @@ def list_movies(request):
     return render(request, "movie/list.html", context)
 
 
+@login_required
 def detail_movie(request, pk):
     """Detail of a movie View"""
 
